8e16fe182fe657f8d3301700077c49099f427927
edevPires <133178329+edevPires@users.noreply.github.com>
2023-06-01 12:29:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 function addIten() {
   const text = document.getElementById('input').value;
-  const pElement = document.createElement('p');
+  if (text) {
+    const pElement = document.createElement('p');
 
-  pElement.setAttribute('id', 'element');
-  pElement.setAttribute('onclick', `listHighlight('${text}')`);
+    pElement.setAttribute('id', 'element');
+    pElement.setAttribute('onclick', `listHighlight('${text}')`);
 
-  const elementText = document.createTextNode(text);
-  pElement.appendChild(elementText);
+    const elementText = document.createTextNode(text);
+    pElement.appendChild(elementText);
 
-  const htmlInsertion = document.getElementById('body');
-  htmlInsertion.appendChild(pElement);
+    const htmlInsertion = document.getElementById('body');
+    htmlInsertion.appendChild(pElement);
+  }
+  else
+  {
+    alert('Empty text pompt.');
+  }
+  document.getElementById('input').innerHTML = '';
 }
 
 function listHighlight(text) {
   document.querySelector('.highlight').style.display = "unset";
   document.querySelector('.popup').style.display = "flex";
   document.getElementById('textHighlight').innerHTML = text;
-  // document.getElementById('bodyHTML').style.overflowY = 'hidden';
 }
 
 function removeDiv() {
